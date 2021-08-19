e233d1855e2a863ccea2143cc4026f6886366f8f
ChemiTokens <89020650+Trickw140@users.noreply.github.com>
2021-08-19 21:45:17 +0300

    Create CorsIT.java

---
+++ CorsIT.java
@@ -0,0 +1,55 @@
+package it.io.openliberty.guides.cors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CorsIT {
+
+    String port = System.getProperty("default.http.port");
+    String pathToHost = "http://localhost:" + port + "/";
+
+    @BeforeEach
+    public void setUp() {
+        // JVM does not allow restricted headers by default
+        // Set to true for CORS testing
+        System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+    }
+
+    @Test
+    public void testSimpleCorsRequest() throws IOException {
+        HttpURLConnection connection = HttpUtils.sendRequest(
+                        pathToHost + "configurations/simple", "GET",
+                        TestData.simpleRequestHeaders);
+        checkCorsResponse(connection, TestData.simpleResponseHeaders);
+
+        printResponseHeaders(connection, "Simple CORS Request");
+    }
+
+
+    public void checkCorsResponse(HttpURLConnection connection,
+                    Map<String, String> expectedHeaders) throws IOException {
+        assertEquals(200, connection.getResponseCode(), "Invalid HTTP response code");
+        expectedHeaders.forEach((responseHeader, value) -> {
+            assertEquals(value, connection.getHeaderField(responseHeader),
+                            "Unexpected value for " + responseHeader + " header");
+        });
+    }
+
+    public static void printResponseHeaders(HttpURLConnection connection,
+                    String label) {
+        System.out.println("--- " + label + " ---");
+        Map<String, java.util.List<String>> map = connection.getHeaderFields();
+        for (Entry<String, java.util.List<String>> entry : map.entrySet()) {
+            System.out.println("Header " + entry.getKey() + " = " + entry.getValue());
+        }
+        System.out.println();
+    }
+
+}
